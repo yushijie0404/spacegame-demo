17e2ab3b6756edc179e86476e1a37705f38126b5
YU Shijie <yushijie0404@163.com>
2026-08-17 17:50:07 +0800

    Publish v0.5.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='spacegame-v0.4.0';
+const CACHE_NAME='spacegame-v0.5.8';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/app-icon.svg','./icons/app-icon-180.png','./icons/app-icon-192.png','./icons/app-icon-512.png'];
 
 self.addEventListener('install',event=>{
